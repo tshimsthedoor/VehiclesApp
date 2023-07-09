b22ab40fc42b2ff7d0d224a83dd7845d45e24e64
tresor <tshims79@gmail.com>
2023-07-09 22:34:12 +0200

    Done with my implementation. KlaKlo

--- VehiclesApp/Program.cs
+++ VehiclesApp/Program.cs
@@ -6,7 +6,37 @@ namespace VehiclesApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("*********** Vehicles demo. ***********");
+                Vehicle vehicle = new Boat("B0002");
+                vehicle.DisplayDetails();
+                Console.WriteLine("**************************************");
+
+                ICapability currentCapability = new FloatCapability();
+                vehicle.SetVehicleBehavior(currentCapability);
+                vehicle.DisplayDetails();
+                Console.WriteLine("**************************************");
+
+                vehicle = new Airplane("A0005");
+                currentCapability = new FlyCapability();
+                vehicle.SetVehicleBehavior(currentCapability);
+                vehicle.DisplayDetails();
+                Console.WriteLine("***************************************");
+
+                Console.WriteLine("Adding float behavior to the airplane.");
+                // Adding float capability to an airplane
+                currentCapability = new FloatCapability();
+                vehicle.SetVehicleBehavior(currentCapability);
+                vehicle.DisplayDetails();
+                Console.WriteLine("****************************************");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+                
+            }
         }
     }
 }
