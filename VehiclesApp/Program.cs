950f8f6489859e903a8b5a73311fae3011108ed7
tresor <tshims79@gmail.com>
2023-07-09 21:26:52 +0200

    Add project files.

---
+++ VehiclesApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehiclesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
