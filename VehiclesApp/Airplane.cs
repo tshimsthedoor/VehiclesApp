f13c200fce0e0524af808a5fb362cd2a54fb79ae
tresor <tshims79@gmail.com>
2023-07-09 21:30:52 +0200

    Added two classes Boat and Airplane both inheriting from vehicle

---
+++ VehiclesApp/Airplane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VehiclesApp
+{
+    class Airplane : IVehicle
+    {
+        public void Float()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("I liek to fly");
+        }
+    }
+}
---
+++ VehiclesApp/Boat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehiclesApp
+{
+    class Boat : IVehicle
+    {
+        public void Float()
+        {
+            Console.WriteLine("I liek to float");
+        }
+
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
